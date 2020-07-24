b944e3a2568db1f1e1985acc685af1f713cd03ff
jaeny96 <42085171+jaeny96@users.noreply.github.com>
2020-07-24 23:21:04 +0900

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,6 @@
 module.exports = {
+  extends: ["airbnb-base", "plugin:prettier/recommended"],
   env: {
     browser: true,
-    es6: true,
-    node: true,
-  },
-  extends: ["airbnb-base", "plugin:prettier/recommended"],
-  globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: "module",
-  },
-  rules: {
-    "no-console": "off",
-    "prettier/prettier": "off",
   },
 };
