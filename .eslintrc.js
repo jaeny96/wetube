e8734de88367f2726cb96cdb1b2d3c4fa768cbc8
jaeny96 <42085171+jaeny96@users.noreply.github.com>
2020-07-25 00:46:28 +0900

    Prestart

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,11 @@ module.exports = {
   extends: ["airbnb-base", "plugin:prettier/recommended"],
   env: {
     browser: true,
+    es6: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
   },
 };
